89477fe1ab121f4af13ed0c42bd7d43138c0574d
Compu12 <xavier.canizares.95@gmail.com>
2021-03-04 18:26:18 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+//librerias
+import { Component } from "@angular/core";
+//decorador metadatos
+@Component({
+selector: "app-dashboard",
+templateUrl:'./dashboard.component.html',
+styleUrls:['./dashboard.component.css']
+
+
+})
+//clase de ejecucion del componente
+export class DashboardComponent {
+
+    constructor() {
+    
+    }
+}
