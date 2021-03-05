d67a7b9e4406daed82685c0f7f77044f8aa622ee
Compu12 <xavier.canizares.95@gmail.com>
2021-03-05 15:31:03 -0500

    dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular003';
+  dadoIzquierdo:string='../assets/imagenes/dado1.png';
+  dadoDerecho:string='../assets/imagenes/dado2.png';
+  numeroInferior:number =1;
+  numeroSuperior:number=2;
+
+
+  lanzarDados():void{
+    this.numeroInferior=Math.round( Math.random()*5)+1;
+    this.numeroSuperior=Math.round( Math.random()*5)+1;
+
+    this.dadoIzquierdo='../assets/imagenes/dado'+this.numeroInferior+'.png';
+    this.dadoDerecho='../assets/imagenes/dado'+this.numeroSuperior+'.png';
+    console.log(this.numeroInferior,this.numeroSuperior);
+
+  }
+  
 }
